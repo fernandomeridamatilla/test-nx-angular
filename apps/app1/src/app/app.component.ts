5e287f0dbe1423f0154d33c7cca4deeef3026a23
Fernando Merida Matilla <fernandomeridamatilla@gmail.com>
2022-09-12 18:32:35 +0200

    feat: add and configure stylelint. create vscode config with recommended extensions and settings

--- apps/app1/src/app/app.component.ts
+++ apps/app1/src/app/app.component.ts
@@ -2,13 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app1-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
   title = 'app1';
-
-  constructor() {
-    console.log('test commitlint');
-  }
 }
